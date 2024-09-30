c6671816815481514447ffa1d31883956a483290
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-09-30 08:54:33 +0200

    Добавьте файлы проекта.

---
+++ Test/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Person
+    {
+        private int age = 1;
+        public int Age
+        {
+            set
+            {
+                if (value < 1 || value > 120)
+                    Console.WriteLine("Возраст должен быть в диапазоне от 1 до 120");
+                else
+                    age = value;
+            }
+            get { return age; }
+        }
+    }
+}
